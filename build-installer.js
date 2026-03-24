1d0d2080839a0d853334ba95a7295c559cd4ef40
阿修罗 <whndeweilai@163.com>
2026-03-24 22:22:18 +0800

    更新：增加打包脚本

---
+++ build-installer.js
@@ -0,0 +1,27 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller;
+const path = require('path');
+
+async function createInstaller() {
+  try {
+    console.log('正在生成Windows安装包...');
+
+    await createWindowsInstaller({
+      appDirectory: path.join(__dirname, 'dist', 'shutdown-cron-win32-x64'),
+      outputDirectory: path.join(__dirname, 'dist', 'installer'),
+      authors: 'Your Name',
+      exe: 'shutdown-cron.exe',
+      name: 'ShutdownCron',
+      description: '定时关机软件',
+      version: require('./package.json').version,
+      noMsi: false,
+      setupExe: '定时关机软件安装包.exe'
+    });
+
+    console.log('Windows安装包生成成功！');
+  } catch (error) {
+    console.error('生成安装包失败:', error.message);
+    process.exit(1);
+  }
+}
+
+createInstaller();
